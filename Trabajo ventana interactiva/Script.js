c457b9fa981cc4809bf8389e42f7db87ae703e30
Juana5123 <jvbermudez@ucompensar.edu.co>
2025-09-22 23:45:09 -0500

    Add files via upload

---
+++ Trabajo ventana interactiva/Script.js
@@ -0,0 +1,17 @@
+const buttonAbrir = document.getElementById("abrirModalBtn");
+const buttonCerrar = document.getElementById("cerrarModalBtn");
+const overlay = document.getElementById("modalOverlay");
+
+buttonAbrir.addEventListener("click", function(){
+    overlay.classList.add("visible");
+});
+
+buttonCerrar.addEventListener("click", function(){
+    overlay.classList.remove("visible");
+});
+
+overlay.addEventListener("click", function(e){
+    if(e.target === overlay){
+        overlay.classList.remove("visible");
+    }
+});
